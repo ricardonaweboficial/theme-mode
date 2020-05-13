0fb387e1c6e3714d8cd24d4ff45a9d7c05b9e8fc
RicardoWorf <steve4137@gmail.com>
2020-05-13 19:41:00 -0300

    Themes dark and light

---
+++ web/src/themes/dark.js
@@ -0,0 +1,10 @@
+export default {
+	title: 'dark',
+
+	colors: {
+		primary: '#001B42',
+
+		background: '#0f0f0f',
+		color:'#fff',
+	}
+}
---
+++ web/src/themes/light.js
@@ -0,0 +1,10 @@
+export default {
+	title: "light",
+
+	colors: {
+		primary: '#00C10C',
+
+		background: '#f1f1f1',
+		color:'#333'
+	}
+}
